5d0d9bee5d3985e589b93efc60e3536fce01eecc
bellapalumbi <bpalumbi@youth.dhftc.org>
2016-07-06 15:46:26 -0400

    Checks to see if the color inputed was valid. If not, tells the user so.

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-var myColor = prompt("Please input your favorite color.");
-//console.log(myColor);
-
 var myDiv1 = document.getElementById("div1");
 var myDiv2 = document.getElementById("div2");
 var myDiv3 = document.getElementById("div3");
 var myMainText = document.getElementById("textDiv");
 
-myDiv1.style.borderColor = myColor;
-myDiv2.style.borderColor = myColor;
-myDiv3.style.borderColor = myColor;
+var mainFunction() {
+    var myColor = prompt("Please input your favorite color.");
+    
+    myDiv1.style.borderColor = myColor;
+    myDiv2.style.borderColor = myColor;
+    myDiv3.style.borderColor = myColor;
 
-myMainText.style.color = myColor;
-myMainText.style.borderTopColor = 'black';
-myMainText.style.borderLeftColor = 'black';
-myMainText.style.borderRightColor = 'black';
+    myMainText.style.color = myColor;
+    myMainText.style.borderTopColor = 'black';
+    myMainText.style.borderLeftColor = 'black';
+    myMainText.style.borderRightColor = 'black';
+}
+
+if (myDiv1.style.borderColor == false) {
+    alert("The color you inputed was not valid. Refresh the page.")
+}
+
